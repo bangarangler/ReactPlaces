f8bed507d16614f25a23a1e183ecef8e0050627d
jonpalacio <jonpalacio@pop-os.localdomain>
2019-11-30 13:38:49 -0500

    added UsersList and UserItem component

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-  return <h2>Users Working...</h2>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Jon Palacio",
+      image:
+        "https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+      places: 3
+    }
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
