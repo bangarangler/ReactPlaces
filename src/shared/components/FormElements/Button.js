a40a8be403018e58193517fe845d8f8ac1ec9e50
jonpalacio <jonpalacio@pop-os.localdomain>
2019-12-01 10:37:46 -0500

    Created Modal Component and added a portal div for it in index.html.  hooked it up with some styling

--- src/shared/components/FormElements/Button.js
+++ src/shared/components/FormElements/Button.js
@@ -30,7 +30,9 @@ const Button = props => {
     <button
       className={`button button--${props.size || "default"} ${props.inverse &&
         "button--inverse"} ${props.danger && "button--danger"}`}
-      type={props.type}>
+      type={props.type}
+      onClick={props.onClick}
+      disabled={props.disabled}>
       {props.children}
     </button>
   );
