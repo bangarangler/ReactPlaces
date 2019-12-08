867006da72e9c87c769a66c566e12861ad3c4608
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-12-08 10:19:17 -0500

    Added logout button based on context...
    on click will logout a user and navigate you back to login

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -1,11 +1,11 @@
-import React, { useContext } from "react";
-import { NavLink } from "react-router-dom";
+import React, {useContext} from 'react';
+import {NavLink} from 'react-router-dom';
 
-import { AuthContext } from '../../context/auth-context';
-import "./NavLinks.css";
+import {AuthContext} from '../../context/auth-context';
+import './NavLinks.css';
 
 const NavLinks = props => {
-  const auth = useContext(AuthContext)
+  const auth = useContext(AuthContext);
 
   return (
     <ul className="nav-links">
@@ -15,19 +15,24 @@ const NavLinks = props => {
         </NavLink>
       </li>
       {auth.isLoggedIn && (
-      <li>
-        <NavLink to="/u1/places">MY PLACES</NavLink>
-      </li>
+        <li>
+          <NavLink to="/u1/places">MY PLACES</NavLink>
+        </li>
       )}
       {auth.isLoggedIn && (
-      <li>
-        <NavLink to="/places/new">ADD PLACE</NavLink>
-      </li>
+        <li>
+          <NavLink to="/places/new">ADD PLACE</NavLink>
+        </li>
       )}
       {!auth.isLoggedIn && (
-      <li>
-        <NavLink to="/auth">AUTHENTICATE</NavLink>
-      </li>
+        <li>
+          <NavLink to="/auth">AUTHENTICATE</NavLink>
+        </li>
+      )}
+      {auth.isLoggedIn && (
+        <li>
+          <button onClick={auth.logout}>LOGOUT</button>
+        </li>
       )}
     </ul>
   );
