3cb92e9583d5b9df4e045865636078bd75b087ff
Jonathan Dain Palacio <bangarangler@gmail.com>
2020-01-01 18:16:44 -0500

    front end image picker native browser api...
    up and working

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -39,7 +39,8 @@ const Auth = () => {
       setFormData(
         {
           ...formState.inputs,
-          name: undefined
+          name: undefined,
+          image: undefined
         },
         formState.inputs.email.isValid && formState.inputs.password.isValid
       );
@@ -49,6 +50,10 @@ const Auth = () => {
           ...formState.inputs,
           name: {
             value: "",
+            isValid: false
+          },
+          image: {
+            value: null,
             isValid: false
           }
         },
@@ -60,6 +65,8 @@ const Auth = () => {
 
   const authSubmitHandler = async e => {
     e.preventDefault();
+
+    console.log(formState.inputs);
 
     if (isLoginMode) {
       try {
@@ -115,7 +122,9 @@ const Auth = () => {
               onInput={inputHandler}
             />
           )}
-          {!isLoginMode && <ImageUpload center id="image" />}
+          {!isLoginMode && (
+            <ImageUpload center id="image" onInput={inputHandler} />
+          )}
           <Input
             element="input"
             id="email"
